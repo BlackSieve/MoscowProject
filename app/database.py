2b88becf00dcda189562a631c982dc55aea94446
BlackSieve <ivansarinov25@gmail.com>
2024-10-23 19:36:23 +0300

    Создана БД

--- app/database.py
+++ app/database.py
@@ -3,11 +3,19 @@ import sqlite3 as sq
 db = sq.connect('tg.bd')
 cur = db.cursor()
 
-
 async def db_start():
-    cur.execute("CREATE TABLE IF NOT EXISTS accounts(id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                "cart_id TEXT)")
 
-    cur.execute("CREATE TABLE IF NOT EXISTS feedback(i_id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                "feed TEXT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS profile(user_id TEXT PRIMARY KEY, feed TEXT")
     db.commit()
+
+
+async def create_profile(user_id):
+    user = cur.execute("SELECT 1 FROM profile WHERE user_id == '{key}'".format(key=user_id))
+    if not user:
+        cur.execute("INSERT INTO profile VALUES(?,?,?,?,?)", (user_id,'','','',''))
+        db.commit()
+
+async def edit_profile(state,user_id):
+    async with state.proxy() as data:
+        cur.execute("UPDATE profile WHERE user_id=='{}' SET feed == '{}',".format(user_id,data['feed']))
+        db.commit()
