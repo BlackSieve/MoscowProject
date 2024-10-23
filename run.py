1650f0197dd0da7b5c194b03c5aa7be192dbd953
BlackSieve <ivansarinov25@gmail.com>
2024-10-23 19:35:39 +0300

    Внесены роутеры

--- run.py
+++ run.py
@@ -1,17 +1,27 @@
 import asyncio
-from aiogram import Bot,Dispatcher
+from aiogram import Bot, Dispatcher
+
+from app.database import db_start
 from keyboard.handlers import router
+from game.quize_game import game
 from dotenv import load_dotenv
 import os
+
 
 load_dotenv()
 bot = Bot(os.getenv('TOKEN'))
 dp = Dispatcher()
 
 
+async def startup(_):
+    await db_start()
+    print('Бот запущен!')
+
+
 async def main():
-    dp.include_router(router)
-    await dp.start_polling(bot)
+    dp.include_routers(router,game)
+    await dp.start_polling(bot, on_startup=startup)
+
 
 if __name__ == '__main__':
     asyncio.run(main())
